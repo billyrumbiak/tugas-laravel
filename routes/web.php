69b19efa82225d6dbd6c252327af40dcd210757d
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2024-03-21 03:08:46 +0900

    Login, Logout, Register dan Middleware

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login-proses',[LoginController::class,'login_proses'])->name('login-proses');
+Route::get('/logout',[LoginController::class,'logout'])->name('logout');
+
+Route::get('/register',[LoginController::class,'register'])->name('register');
+Route::post('/register-proses',[LoginController::class,'register_proses'])->name('register-proses');
+
+Route::group(['prefix' => 'admin','middleware' => ['auth'], 'as' => 'admin.'] , function(){
+    Route::get('/dashboard',[HomeController::class,'dashboard'])->name('dashboard');
+
+    Route::get('/user',[HomeController::class,'index'])->name('index');
+    Route::get('/create',[HomeController::class,'create'])->name('user.create');
+    Route::post('/store',[HomeController::class,'store'])->name('user.store');
+
+    Route::get('/edit/{id}',[HomeController::class,'edit'])->name('user.edit');
+    Route::put('/update/{id}',[HomeController::class,'update'])->name('user.update');
+    Route::delete('/delete/{id}',[HomeController::class,'delete'])->name('user.delete');
+});
+
